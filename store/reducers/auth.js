2c9919d38c940abc3b0840b6381bb88ccde30bce
Xenus90 <xenus90@gmail.com>
2020-03-10 19:01:53 +0200

    update to Navigation V5

---
+++ store/reducers/auth.js
@@ -0,0 +1,35 @@
+import { AUTHENTICATE, LOGOUT, SET_DID_TRY_AUTO_LOGIN } from '../actions/auth';
+
+const initialState = {
+    token: null,
+    userId: null,
+    didTryAutoLogin: false
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case AUTHENTICATE:
+            return {
+                token: action.token,
+                userId: action.userId,
+                didTryAutoLogin: true
+            };
+        case LOGOUT:
+            return {
+                ...initialState,
+                didTryAutoLogin: true
+            };
+        // case SIGNUP:
+        //   return {
+        //     token: action.token,
+        //     userId: action.userId
+        //   };
+        case SET_DID_TRY_AUTO_LOGIN:
+            return {
+                ...state,
+                didTryAutoLogin: true
+            };
+        default:
+            return state;
+    }
+};
